bd6e1cbc11ad2dfc0bd649ffb795c05f28d5c54b
Manjunath Baburao <manjunathbaburao@users.noreply.github.com>
2018-09-27 12:52:25 +0530

    Encrypted Twitter API keys

--- tweet_comments/config/config.js
+++ tweet_comments/config/config.js
@@ -2,8 +2,8 @@
  * Configuration to connect to Twitter account - Credentials needed
  */
 module.exports = {
- consumer_key:         'KSxIPvCqosuP5XUQlhtFANgvm',
- consumer_secret:      'TRLSlROmHFJBqeXivKu49r2NsdSNv75D671ZCpI4w43cXDUXmN',
- access_token:         '891350594518040576-faZi2tsZ91EBLHYcPnDf7zQfzAlp5eQ',
- access_token_secret:  'txogLkrcIS2Ey2jNVjGHFmZmuPsqzGLauCahelYRbzFyp'
+ consumer_key:         '<fill from file>',
+ consumer_secret:      '<fill from file>'
+ access_token:         '<fill from file>'
+ access_token_secret:  '<fill from file>'
 };
